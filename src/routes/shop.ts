eefad1221a6f650852eb1cedc3bd26cc1e138f40
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2021-12-15 03:31:15 +0900

    🎨 가게 주문 목록 조회 순서 변경

--- src/routes/shop.ts
+++ src/routes/shop.ts
@@ -17,6 +17,9 @@ router.get("/around", retrieve.getAroundShopList);
 // 가게 구독 조회 API
 router.get("/:shopId/subscriptions", retrieve.getShopSubsList);
 
+// 가게 주문 목록 조회 API
+router.get("/:shopId/orders", order.getShopOrderList);
+
 // 가게 정보 관련 API
 router.get("/:shopId", info.getShop);
 router.post("/create", info.createShop);
@@ -36,6 +39,3 @@ router.get("/:shopId/posts/:postId", post.getShopPostDetail);
 router.post("/:shopId/posts/create", post.createShopPost);
 router.patch("/:shopId/posts/:postId", post.updateShopPost);
 router.delete("/:shopId/posts/:postId", post.deleteShopPost);
-
-// 가게 주문 목록 조회 API
-router.get("/:shopId/orders", order.getShopOrderList);
