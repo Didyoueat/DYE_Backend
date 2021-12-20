9cec2a66600730145524adf06e07639051182232
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2021-12-20 20:17:28 +0900

    ⚡️ Property Check 모듈 수정

--- src/modules/property.ts
+++ src/modules/property.ts
@@ -1,5 +1,7 @@
-import { infoTypes } from "infoTypes";
 import { ObjectType, getCustomRepository } from "typeorm";
+import httpStatus from "http-status";
+import ApiError from "@modules/apiError";
+import { infoTypes } from "infoTypes";
 
 /**
  * Custom Repository
@@ -10,44 +12,51 @@ import { ObjectType, getCustomRepository } from "typeorm";
 export const repository = <T>(repository: ObjectType<T>) => getCustomRepository(repository);
 
 /**
- * 데이터 유효성 검사
+ * 객체 빈 key = undefined 조합
  * @param data
- * @returns boolean
+ * @returns
  */
 
-const isProperty = (data: infoTypes.shop | infoTypes.dish | infoTypes.subscription, value: any): boolean =>
-    data.hasOwnProperty(value);
+const shopProp: string[] = [
+    "businessName",
+    "businessNumber",
+    "businessPhone",
+    "password",
+    "dayOff",
+    "latitude",
+    "longitude",
+    "name",
+    "phone",
+    "origin",
+    "content",
+    "officeHour",
+    "temporaryDayStart",
+    "temporaryDayEnd",
+];
 
-export const shopPropertyCheck = (data: infoTypes.shop): boolean => {
-    return (
-        isProperty(data, data.businessNumber) &&
-        isProperty(data, data.businessName) &&
-        isProperty(data, data.businessPhone) &&
-        isProperty(data, data.dayOff) &&
-        isProperty(data, data.password) &&
-        isProperty(data, data.address) &&
-        isProperty(data, data.latitude) &&
-        isProperty(data, data.longitude) &&
-        isProperty(data, data.name) &&
-        isProperty(data, data.phone) &&
-        isProperty(data, data.origin) &&
-        isProperty(data, data.content) &&
-        isProperty(data, data.officeHour) &&
-        isProperty(data, data.temporaryDayStart) &&
-        isProperty(data, data.temporaryDayEnd)
-    );
-};
+const dishProp: string[] = ["main", "thumbnail", "title", "content", "price", "count", "weight"];
 
-export const dishPropertyCheck = (data: infoTypes.dish): boolean => {
-    return (
-        isProperty(data, data.main) &&
-        isProperty(data, data.thumbnail) &&
-        isProperty(data, data.title) &&
-        isProperty(data, data.content) &&
-        isProperty(data, data.price) &&
-        isProperty(data, data.count) &&
-        isProperty(data, data.weight)
-    );
+const subsProp: string[] = [
+    "shopId",
+    "weekLabel",
+    "reciever",
+    "address",
+    "paymentState",
+    "deliveryCost",
+    "toshop",
+    "toDelivery",
+    "dishes",
+];
+
+export const addProperty = (data: infoTypes.shop | infoTypes.dish | infoTypes.subscription, type: string) => {
+    const prop: string[] = type === "shop" ? shopProp : type === "dish" ? dishProp : type === "subscription" ? subsProp : [];
+
+    for (let i = 0; i < prop.length; i++) {
+        if (data[prop[i]] === undefined) {
+            data[prop[i]] = undefined;
+        }
+    }
+    return data;
 };
 
 /**
@@ -56,9 +65,11 @@ export const dishPropertyCheck = (data: infoTypes.dish): boolean => {
  * @returns boolean
  */
 
-export const shopTypeCheck = (data: infoTypes.shop): boolean => {
+const shopTypeCheck = (data: infoTypes.shop): boolean => {
     for (const key in data) {
-        if (!(key === "origin" || key === "content" || key === "temporaryDayStart" || key === "temporaryDayEnd")) {
+        if (data[key] === undefined) {
+            continue;
+        } else if (!(key === "origin" || key === "content" || key === "temporaryDayStart" || key === "temporaryDayEnd")) {
             if (key === "dayOff" || key === "latitude" || key === "longitude") {
                 if (isNaN(data[key])) return false;
             } else {
@@ -76,9 +87,11 @@ export const shopTypeCheck = (data: infoTypes.shop): boolean => {
     return true;
 };
 
-export const dishTypeCheck = (data: infoTypes.dish): boolean => {
+const dishTypeCheck = (data: infoTypes.dish): boolean => {
     for (const key in data) {
-        if (key === "price" || key === "count" || key === "weight") {
+        if (data[key] === undefined) {
+            continue;
+        } else if (key === "price" || key === "count" || key === "weight") {
             if (isNaN(data[key])) return false;
         } else if (key === "main" || key === "thumbnail") {
             if (typeof data[key] !== "boolean") return false;
@@ -92,4 +105,80 @@ export const dishTypeCheck = (data: infoTypes.dish): boolean => {
     }
 
     return true;
+};
+
+const subsTypeCheck = (data: infoTypes.subscription): boolean => {
+    for (const key in data) {
+        if (data[key] === undefined) {
+            continue;
+        } else if (key === "shopId" || key === "weekLabel" || key === "deliveryCost") {
+            if (isNaN(data[key])) return false;
+        } else if (key === "dishes") {
+            const dishes: Array<infoTypes.subscriptionDish> = data[key];
+            const option: string[] = ["dishId", "orderCount"];
+
+            if (dishes.length === 0) return false;
+            for (let i = 0; i < dishes.length; i++) {
+                if (JSON.stringify(option.sort()) !== JSON.stringify(Object.keys(dishes[i]).sort())) return false;
+                if (isNaN(dishes[i].dishId) || isNaN(dishes[i].orderCount)) return false;
+            }
+        } else {
+            if (key === "reciever" || key === "address") {
+                if (typeof data[key] !== "string") return false;
+            } else {
+                if (typeof data[key] !== "string" && data[key] !== null) return false;
+            }
+        }
+    }
+
+    return true;
+};
+
+const changeTypeCheck = (data: infoTypes.changeDish): boolean => {
+    if (data["changeDishes"].length === 0) return false;
+
+    for (let i = 0; i < data["changeDishes"].length; i++) {
+        const { subscriptionDishId, dishId, orderCount } = data["changeDishes"][i];
+        if (isNaN(subscriptionDishId) || isNaN(dishId) || isNaN(orderCount)) return false;
+    }
+
+    return true;
+};
+
+/**
+ * 데이터 분리
+ * @param data
+ * @returns boolean
+ */
+
+const typeSeparate = (data: infoTypes.shop | infoTypes.dish | infoTypes.subscription | infoTypes.changeDish): boolean => {
+    if ("businessName" in data) {
+        return shopTypeCheck(data);
+    } else if ("thumbnail" in data) {
+        return dishTypeCheck(data);
+    } else if ("reciever" in data) {
+        return subsTypeCheck(data);
+    } else if ("changeDishes" in data) {
+        return changeTypeCheck(data);
+    } else {
+        return false;
+    }
+};
+
+/**
+ * 타입 체크
+ * @param arg
+ */
+
+export const propertyCheck = (...arg: any[]): void => {
+    for (let i = 0; i < arg.length; i++) {
+        if (typeof arg[i] === "number") {
+            if (isNaN(arg[i])) throw new ApiError(httpStatus.BAD_REQUEST, "잘못된 요청입니다.");
+        } else {
+            if (!typeSeparate(arg[i])) throw new ApiError(httpStatus.BAD_REQUEST, "잘못된 요청입니다.");
+            for (const key in arg[i]) {
+                if (arg[i][key] === undefined) delete arg[i][key];
+            }
+        }
+    }
 };
