34ac5b796eaf696fb0f8cc1460f618b64c98a094
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2022-02-16 01:27:25 +0900

    DYE-204 ✨ Swagger API 모듈 추가

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,19 @@
 import { Router } from "express";
+import swaggerUI from "swagger-ui-express";
+import YAML from "yamljs";
+import dir from "path";
+
 import * as authRouter from "@routes/auth";
 import * as userRouter from "@routes/user";
 import * as adminRouter from "@routes/admin";
 import * as shopRouter from "@routes/shop";
 
+const swaggerSpec = YAML.load(dir.join(__dirname, "/../swagger/openapi.yaml"));
+
 export const path: string = "/v0";
 export const router: Router = Router();
 
+router.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 router.use(authRouter.path, authRouter.router);
 router.use(userRouter.path, userRouter.router);
 router.use(adminRouter.path, adminRouter.router);
