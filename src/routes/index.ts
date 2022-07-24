41f2c87c6e8f7792c3587b2bca3b845574d5b84c
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2022-07-24 21:53:27 +0900

    fix: swagger docs 안 열리는 현상 수정

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,19 +1,13 @@
 import { Router } from "express";
-import swaggerUI from "swagger-ui-express";
-import YAML from "yamljs";
-import appDir from "app-root-path";
 
 import * as authRouter from "@routes/auth";
 import * as userRouter from "@routes/user";
 import * as adminRouter from "@routes/admin";
 import * as shopRouter from "@routes/shop";
 
-const swaggerSpec = YAML.load(appDir.path + "/src/swagger/openapi.yaml");
-
 export const path: string = "/v0";
 export const router: Router = Router();
 
-router.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 router.use(authRouter.path, authRouter.router);
 router.use(userRouter.path, userRouter.router);
 router.use(adminRouter.path, adminRouter.router);
